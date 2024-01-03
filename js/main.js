ba29fb5e47ab02ba5b6c6c3fb70c7649f721c10d
Kamal <kamaladebayo10@gmail.com>
2024-01-03 20:26:00 +0100

    First draft

--- js/main.js
+++ js/main.js
@@ -28,3 +28,21 @@ let clickEvent = () => {
 sidebar_link.forEach((item) => {
     item.addEventListener('click', clickEvent)
 });
+
+
+// Sponsors slider
+const slider = document.querySelector('.slider');
+const logos = document.querySelectorAll('.slider img');
+
+let index = 0;
+
+function slide() {
+    index++;
+    if (index >= logos.length) {
+        index = 0;
+    }
+    const offset = -index * logos[0].clientWidth;
+    slider.style.transform = `translateX(${offset}px)`;
+}
+
+setInterval(slide, 1000); // Change slide every 5 seconds (adjust as needed)
